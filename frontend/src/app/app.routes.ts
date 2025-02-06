7e9c09eb17f8b2935128010f47c53076c4ca5edb
Hubert <kiszkahub@gmail.com>
2025-02-06 17:02:49 +0100

    Main page works

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from './auth/register/register.component';
 import {LoginComponent} from './auth/login/login.component';
+import {MainPageCombinedComponent} from './mainPage/main-page-combined/main-page-combined.component';
 
 export const routes: Routes = [
+  { path: '', component: MainPageCombinedComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
 ];
