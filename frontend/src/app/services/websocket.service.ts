d7566c2629130d6670ddc63fc9961d8ae6b035e4
Hubert <kiszkahub@gmail.com>
2025-02-10 20:52:03 +0100

    ..

--- frontend/src/app/services/websocket.service.ts
+++ frontend/src/app/services/websocket.service.ts
@@ -1,23 +1,28 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Client, Message } from '@stomp/stompjs';
-import * as SockJS from 'sockjs-client';
-import {BehaviorSubject, Observable} from 'rxjs';
+import SockJS from 'sockjs-client';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-export interface ChangePayload{
-  payloadContent: string,
-  payloadIndex: number
+export interface ChangePayload {
+  payloadContent: string;
+  payloadIndex: number;
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class WebsocketService{
+export class WebsocketService {
   private client: Client;
-  private messagesSubject = new BehaviorSubject<ChangePayload>([]);
-  private messages$ = this.messagesSubject.asObservable();
+  private messagesSubject = new BehaviorSubject<ChangePayload>({
+    payloadContent: '',
+    payloadIndex: 0
+  });
+  public messages$ = this.messagesSubject.asObservable();
   constructor() {
     this.client = new Client({
-      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+      webSocketFactory: () => {
+        return new SockJS('http://localhost:8080/ws');
+      },
       debug: (str) => {
         console.log(str);
       },
@@ -25,29 +30,32 @@ export class WebsocketService{
       heartbeatIncoming: 4000,
       heartbeatOutgoing: 4000,
     });
-    this.client.onStompError = (frame) => {
-      console.error('Broker reported error: ' + frame.headers['message']);
-      console.error('Additional details: ' + frame.body);
+    this.client.onConnect = () => {
+      console.log('Conn established');
+      this.client.subscribe('/topic/messages', (message: Message) => {
+        const newMessage: ChangePayload = JSON.parse(message.body);
+        this.messagesSubject.next(newMessage);
+      });
     };
-
+    this.client.onStompError = (frame) => {
+      console.error(frame.headers['message']);
+      console.error(frame.body);
+    };
     this.client.activate();
   }
-
-  sendMessage(content: string): void {
+  sendMessage(payload: ChangePayload): void {
     if (this.client && this.client.connected) {
       this.client.publish({
         destination: '/app/send',
-        body: JSON.stringify({ content })
+        body: JSON.stringify(payload)
       });
     } else {
-      console.error('Not connected');
+      console.error('Conn err');
     }
   }
-
   disconnect(): void {
     if (this.client) {
       this.client.deactivate();
     }
   }
 }
-
